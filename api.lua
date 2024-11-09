35e9487113353552e6fa3e0f3ce2a4f4bb89d589
sparky4 <sparky4@cock.li>
2024-11-09 17:00:46 -0600

    added 4 characters so it would work again with fachwerk_additions

--- api.lua
+++ api.lua
@@ -5,7 +5,7 @@
 function fachwerk.register_fachwerk(basename, texture, description, craft_from)
 	local group_def = {choppy = 2, oddly_breakable_by_hand = 2, cracky = 3}
 
-	minetest.register_node("fachwerk:" .. basename, {
+	minetest.register_node(":fachwerk:" .. basename, {
 		description = "Timber-framed " .. description,
 		tiles = {texture .. "^fachwerk_blank.png"},
 		paramtype = "light",
@@ -14,7 +14,7 @@ function fachwerk.register_fachwerk(basename, texture, description, craft_from)
 		sounds = default.node_sound_stone_defaults()
 	})
 
-	minetest.register_node("fachwerk:" .. basename .. "_1", {
+	minetest.register_node(":fachwerk:" .. basename .. "_1", {
 		description = "Timber-framed " .. description .. " Oblique Beam 1",
 		tiles = {
 			texture .. "^fachwerk_blank.png", -- top
@@ -31,7 +31,7 @@ function fachwerk.register_fachwerk(basename, texture, description, craft_from)
 	})
 
 	-- TODO: is this one really needed? the node above covers most of that already
-	minetest.register_node("fachwerk:" .. basename .. "_2", {
+	minetest.register_node(":fachwerk:" .. basename .. "_2", {
 		description = "Timber-framed " .. description .. " Oblique Beam 2",
 		tiles = {
 			texture .. "^fachwerk_blank.png", -- top
@@ -47,7 +47,7 @@ function fachwerk.register_fachwerk(basename, texture, description, craft_from)
 		sounds = default.node_sound_stone_defaults()
 	})
 
-	minetest.register_node("fachwerk:" .. basename .. "_cross", {
+	minetest.register_node(":fachwerk:" .. basename .. "_cross", {
 		description = "Timber-framed " .. description .. " Cross",
 		tiles = {texture .. "^fachwerk_cross.png"},
 		groups = group_def,
